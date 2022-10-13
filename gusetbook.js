3699c0d0a73f3fba9ff49b73d10cd8659cc77a50
DESKTOP-SPTR1S3\손은진 <dojeon032@gmail.com>
2022-10-14 01:27:16 +0900

    js_3
    main에 to do list 추가, 입력값이 to do list에 추가되도록 js 걸기
    gusetbook의 일부 클래스명 변경에 따라 js 변수 변경, 섹션 병합

--- gusetbook.js
+++ gusetbook.js
@@ -3,25 +3,26 @@ function sup() {
 	alert('Clap!')
 }
 
-const addRe = document.querySelector('#re');
-addRe.addEventListner('click', () => {
-	const gb = document.querySelector('#gb'); //입력창 변수로 불러오기 
-	const text = input.value.trim(); // 입력 받아오기
+const addRe = document.querySelector('#add-re');
 
-	if (text !== '') {
-		addGueBo(text);
-		input.value = ''; // 빈칸으로 리셋
-		input.focus();
-	}
+addRe.addEventListener('click', () => {
+  const GB = document.querySelector('#gb');
+  const text = input.value.trim();
+
+  if (text !== '') {
+    addToRep(text);
+    input.value = '';
+    input.focus();
+  }
 });
 
-function addGueBo(text) {
-	const book = document.querySelector('#book');
+function addToRep(text) {
+  const book = document.querySelector('#book');
 
-	const newBookItem = document.createElement('li'); // 새로운 li 만들기
-	newBookItem.classList.add('book-list'); // 클래스 추가
+  const newBookItem = document.createElement('li');
+  newListItem.classList.add('book-list'); 
 
-	newBookItem.innerHTML = text; // 입력 문자 삽입
+  newListItem.innerHTML = text;
 
-	FileList.appendChild(newBookItem); // 리스트에 새로 만든 book 추가
+  list.appendChild(newListItem);
 }
