4668a8d7a836c635af5e23ebef20b20359d44713
DESKTOP-SPTR1S3\손은진 <dojeon032@gmail.com>
2022-10-14 08:33:24 +0900

    ect_1
    b_write내 입력 게시판 바꾸기(참고 활용)
    main.html에 cal 추가, year. month,date 불러오기

--- main.js
+++ main.js
@@ -30,3 +30,45 @@ function addToList(text) {
 		list.style.textDecoration = "line-through";
 	})
 }
+
+// 달력
+// 연, 월 불러오기
+const date = new Date();
+
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+
+document.querySelector('.year-month').textContent = `${viewYear}. ${viewMonth + 1}.`;
+
+// 날짜 불러오기
+const prevLast = new Date(viewYear, viewMonth, 0);
+const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+const PLDate = prevLast.getDate();
+const PLDay = prevLast.getDay();
+
+const TLDate = thisLast.getDate();
+const TLDay = thisLast.getDay();
+
+const prevDates = [];
+const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+const nextDates = [];
+
+// if문 안에 데이터 배열
+if (PLDay !== 6) {
+    for (let i = 0; i < PLDay + 1; i++) {
+      prevDates.unshift(PLDate - i);
+    }
+  }
+  
+  for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+  }
+
+  const dates = prevDates.concat(thisDates, nextDates);
+
+  dates.forEach((date, i) => {
+    dates[i] = `<div class="date">${date}</div>`;
+  })
+  
+  document.querySelector('.dates').innerHTML = dates.join('');
